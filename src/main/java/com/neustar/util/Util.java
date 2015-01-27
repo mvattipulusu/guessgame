a86abe1e352d5263290b6b44fae6dd33ee56ae82
mvattipulusu <mvattipulusu@crgt.com>
2015-01-27 02:11:00 -0500

    new util file

---
+++ src/main/java/com/neustar/util/Util.java
@@ -0,0 +1,21 @@
+package com.neustar.util;
+
+public class Util {
+	
+	private static final String GUESS_LARGE_THAN_RANDOM = "Your number is too large!";
+	private static final String GUESS_SMALLER_THAN_RANDOM = "Your number is too small!";
+	private static final String GUESS_EQUALS_RANDOM = "You guessed the right number";
+	
+	/**
+	 * Compares Guess with Random No.
+	 * @param guessNo
+	 * @param randomNo
+	 * @return
+	 */
+	public static String compareGuessToRandom(Integer guessNo, Integer randomNo){
+		if(guessNo.equals(randomNo)) return GUESS_EQUALS_RANDOM;
+		return (guessNo > randomNo) ?  GUESS_LARGE_THAN_RANDOM : GUESS_SMALLER_THAN_RANDOM;
+		
+	}
+
+}
